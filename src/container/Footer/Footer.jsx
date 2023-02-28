065ff6ef119ff24207c9f6bf9d1a87d8e28fa83a
Omar-Elshafeey <omarelshafeey1@gmail.com>
2023-02-28 05:53:57 +0200

    first commit

---
+++ src/container/Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+
+import { FooterOverlay, Newsletter } from '../../components';
+import { images } from '../../constants';
+import './Footer.css';
+
+const Footer = () => (
+  <div className="app__footer section__padding" id="login">
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">The restaurant Adress</p>
+        <p className="p__opensans">01000000000</p>
+        <p className="p__opensans">01111111111</p>
+      </div>
+
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer_logo" />
+        <p className="p__opensans">&quot;here is a quote.&quot;</p>
+        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Sutarday-Thursday:</p>
+        <p className="p__opensans">08:00 am - 12:00 am</p>
+        <p className="p__opensans">Friday:</p>
+        <p className="p__opensans">07:00 am - 11:00 pm</p>
+      </div>
+    </div>
+
+    <div className="footer__copyright">
+      <p className="p__opensans">2023 restaurant. All Rights reserved.</p>
+    </div>
+
+  </div>
+);
+
+export default Footer;
---
+++ src/container/index.js
@@ -0,0 +1,15 @@
+import AboutUs from './AboutUs/AboutUs';
+import Chef from './Chef/Chef';
+import FindUs from './Findus/FindUs';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Laurels from './Laurels/Laurels';
+
+export {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Header,
+  Laurels,
+};
